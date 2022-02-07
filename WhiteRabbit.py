c6581409e6b65c6ac6f7ce76ceb5c4e9e88d5529
JesseClift <99159862+JesseClift@users.noreply.github.com>
2022-02-06 23:03:18 -0600

    Update to WhiteRabbit.py
    Changed a bit of the text to remove a curse word that was originally in the script.

--- WhiteRabbit.py
+++ WhiteRabbit.py
@@ -6,5 +6,5 @@ print ("(Back in the Control Center, Arnold is typing at Dennis' workstation.)"
 "Arnold- Access main program grid."
 "(The computer flashes 'PERMISSION DENIED' once more, then the screen suddenly fills with a repeating message: 'YOU DIDN'T SAY THE MAGIC WORD!' At that moment, an animated image of a cartoony Dennis Nedry appears on the other monitor, wagging his finger.)"
 "Computer Nedry- Ah ah ah. You didn't say the magic word. Ah ah ah! Ah ah ah!"
-"Arnold- (livid) PLEASE!!! God damn it! I hate this hacker crap!")
+"Arnold- (livid) PLEASE!!! Darn it! I hate this hacker crap!")
 input()
